e3883bd19d7e05354fe87ea30142d47910de7f19
Brian Sanabria <wnuken@gmail.com>
2014-10-29 14:49:20 -0500

    Cambio url Mediador

--- controller/statics.php
+++ controller/statics.php
@@ -1,5 +1,5 @@
 <?php
 define("QBC_SOAP_SERVER", "http://helios.eltiempo.com.co:8085/Mediador/Servicio.svc?wsdl");
-define("QBC_SOAP_SERVER_PRO", "http://neftis:81/Mediador/Servicio.svc?wsdl");
-define("QBC_SOAP_SERVER_FAKE", "http://nefsstis:81/Mediador/Servicio.svc?wsdl");
+define("QBC_SOAP_SERVER_PRO", "http://neftis.eltiempo.com.co:81/Mediador/Servicio.svc?wsdl");
+define("QBC_SOAP_SERVER_FAKE", "http://neftis:81/Mediador/Servicio.svc?wsdl");
 ?>
